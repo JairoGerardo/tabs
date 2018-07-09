371598d9f13abbf0c0fb5a542ca9586dba225ff6
Jairo <jirogererdo@gmail.com>
2018-07-09 10:36:26 -0500

    Adding TabSelectedListener on Main Activity

--- app/src/main/java/com/example/jairo/tabs/Activities/MainActivity.java
+++ app/src/main/java/com/example/jairo/tabs/Activities/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.example.jairo.tabs.Adapters.PagerAdapter;
 import com.example.jairo.tabs.R;
@@ -27,10 +28,31 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
 
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
         PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
 
         viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Toast.makeText(MainActivity.this, "Selected " + tab.getText(), Toast.LENGTH_SHORT).show();
+                int position = tab.getPosition();
+                viewPager.setCurrentItem(position);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                Toast.makeText(MainActivity.this, "Unselected " + tab.getText(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                Toast.makeText(MainActivity.this, "Reselected " + tab.getText(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
     @Override
